f2e8a85c73e6aeb259371bc3adb370d232cc5f58
Sebastian Alvarez <salvarez@utp.edu.co>
2025-02-20 20:06:49 -0500

    creacion de proyecto

---
+++ src/app/helpers/routes.ts
@@ -0,0 +1,6 @@
+export const routes = [
+    { path: '/exercise1', name: 'Ejercicio 1'},
+    { path: '/exercise2', name: 'Ejercicio 2'},
+    { path: '/shops', name: 'Tiendas'},
+    { path: '/login', name: 'Logueo'},
+]
